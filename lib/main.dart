11ee49514989d924f514627b327a8bb42926d1b1
Rehan-Raza <rehanrazadotcom@gmail.com>
2020-09-13 20:25:21 +0530

    done

---
+++ lib/main.dart
@@ -0,0 +1,23 @@
+import 'package:flutter/material.dart';
+
+void main() {
+  runApp(
+    MaterialApp(
+      debugShowCheckedModeBanner: false,
+      home: Scaffold(
+        body: Center(
+          child: Image(
+            image: AssetImage('android/images/diamond.jpg')
+          ),
+        ),
+        backgroundColor: Colors.blueGrey,
+        appBar: AppBar(
+          title: Center(
+            child: Text('I Am Rich'),
+          ),
+          backgroundColor: Colors.blueGrey[900],
+        ),
+      ),
+    ),
+  );
+}
